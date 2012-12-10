779d5059d01880b0e56d30c702c1bd7e01faee3f
Jeremy <jeremykentc@gmail.com>
2012-12-10 11:19:04 -0500

    adding in support for inserting whole rows

--- Sylvester/Matrix.cs
+++ Sylvester/Matrix.cs
@@ -1,12 +1,16 @@
 ﻿
+using System;
+
 namespace Sylvester
 {
     public class Matrix
     {
         private double[,] _matrix;
+        private int _columns;
         public Matrix(int rows, int columns)
         {
             _matrix = new double[rows,columns];
+            _columns = columns;
             for (var i = 0; i < rows; i++)
             {
                 for (var j = 0; j < columns; j++)
@@ -24,6 +28,14 @@ namespace Sylvester
         public void SetElement(int row, int column, double value)
         {
             _matrix[row, column] = value;
+        }
+
+        public void SetElements(int row, double[] rowValue)
+        {
+            for (var i = 0; i < _columns; i++)
+            {
+                _matrix[row, i] = rowValue[i];
+            }
         }
     }
 }
--- SylvesterTests/MatrixTests.cs
+++ SylvesterTests/MatrixTests.cs
@@ -9,8 +9,11 @@ namespace SylvesterTests
         [Test]
         public void MatrixInitializesToZerosByDefault()
         {
-            var m = new Matrix(2, 3);
+            var m = new Matrix(2, 2);
             Assert.That(m.GetElement(0, 0), Is.EqualTo(0));
+            Assert.That(m.GetElement(0, 1), Is.EqualTo(0));
+            Assert.That(m.GetElement(1, 0), Is.EqualTo(0));
+            Assert.That(m.GetElement(1, 1), Is.EqualTo(0));
         }
 
         [Test]
@@ -19,6 +22,19 @@ namespace SylvesterTests
             var m = new Matrix(2, 2);
             m.SetElement(0, 0, 1);
             Assert.That(m.GetElement(0, 0), Is.EqualTo(1));
+            Assert.That(m.GetElement(0, 1), Is.EqualTo(0));
+            Assert.That(m.GetElement(1, 0), Is.EqualTo(0));
+            Assert.That(m.GetElement(1, 1), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void CanSetRowByRowIndex()
+        {
+            var m = new Matrix(3, 3);
+            m.SetElements(1, new double[3] {1,2,3});
+            Assert.That(m.GetElement(1, 0), Is.EqualTo(1));
+            Assert.That(m.GetElement(1, 1), Is.EqualTo(2));
+            Assert.That(m.GetElement(1, 2), Is.EqualTo(3));
         }
     }
 }
