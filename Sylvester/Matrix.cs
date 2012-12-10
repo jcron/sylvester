aac6577f5f454153e8e99e62bb5ab3c16b5625f8
Jeremy <jeremykentc@gmail.com>
2012-12-10 08:32:41 -0500

    initial work with a couple of unit tests to verify getting and setting of elements

---
+++ Sylvester/Matrix.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+
+namespace Sylvester
+{
+    public class Matrix
+    {
+        private double[,] _matrix;
+        public Matrix(int rows, int columns)
+        {
+            _matrix = new double[rows,columns];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    _matrix[i, j] = 0;
+                }
+            }
+        }
+
+        public double GetElement(int row, int column)
+        {
+            return _matrix[row,column];
+        }
+
+        public void SetElement(int row, int column, double value)
+        {
+            _matrix[row, column] = value;
+        }
+    }
+}
---
+++ SylvesterTests/MatrixTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Sylvester;
+
+namespace SylvesterTests
+{
+    [TestFixture]
+    public class MatrixTests
+    {
+        [Test]
+        public void MatrixInitializesToZerosByDefault()
+        {
+            var m = new Matrix(2, 3);
+            Assert.That(m.GetElement(0, 0), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void CanSetSpecificElements()
+        {
+            var m = new Matrix(2, 2);
+            m.SetElement(0, 0, 1);
+            Assert.That(m.GetElement(0, 0), Is.EqualTo(1));
+        }
+    }
+}
