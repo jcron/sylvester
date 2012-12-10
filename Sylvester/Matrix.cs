22592f892b06ddf41e5be67f4c776edf3ce241d9
Jeremy <jeremykentc@gmail.com>
2012-12-10 13:08:38 -0500

    adding in tests for equality and overloading operators

--- Sylvester/Matrix.cs
+++ Sylvester/Matrix.cs
@@ -1,15 +1,15 @@
 ﻿
-using System;
-
 namespace Sylvester
 {
     public class Matrix
     {
         private double[,] _matrix;
         private int _columns;
+        private int _rows;
         public Matrix(int rows, int columns)
         {
-            _matrix = new double[rows,columns];
+            _matrix = new double[rows, columns];
+            _rows = rows;
             _columns = columns;
             for (var i = 0; i < rows; i++)
             {
@@ -19,7 +19,7 @@ namespace Sylvester
                 }
             }
         }
-
+        
         public double GetElement(int row, int column)
         {
             return _matrix[row,column];
@@ -30,11 +30,87 @@ namespace Sylvester
             _matrix[row, column] = value;
         }
 
-        public void SetElements(int row, double[] rowValue)
+        public void SetRow(int row, double[] rowValue)
         {
             for (var i = 0; i < _columns; i++)
             {
                 _matrix[row, i] = rowValue[i];
+            }
+        }
+        public double[] GetRow(int rowIndex)
+        {
+            var row = new double[_columns];
+            for (var i = 0; i < _columns; i++)
+            {
+                row[i] = _matrix[rowIndex, i];
+            }
+            return row;
+        }
+
+        public void SetColumn(int column, double[] columnValue)
+        {
+            for (var i = 0; i < _rows; i++)
+            {
+                _matrix[i, column] = columnValue[i];
+            }
+        }
+
+        public double[] GetColumn(int columnIndex)
+        {
+            var row = new double[_rows];
+            for (var i = 0; i < _rows; i++)
+            {
+                row[i] = _matrix[i, columnIndex];
+            }
+            return row;
+        }
+
+        public static bool operator ==(Matrix lhs, Matrix rhs)
+        {
+            return lhs.Equals(rhs);
+        }
+
+        public static bool operator !=(Matrix lhs, Matrix rhs)
+        {
+            return !lhs.Equals(rhs);
+        }
+
+        public bool Equals(Matrix other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            if (other._columns == _columns && other._rows == _rows)
+            {
+                for(var i=0; i < _rows; i++)
+                {
+                    for (var j = 0; j < _columns; j++)
+                    {
+                        if (other._matrix[i, j] != _matrix[i, j])
+                            return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != typeof (Matrix)) return false;
+            return Equals((Matrix) obj);
+        }
+
+        // Re-sharper generated
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var result = (_matrix != null ? _matrix.GetHashCode() : 0);
+                result = (result*397) ^ _columns;
+                result = (result*397) ^ _rows;
+                return result;
             }
         }
     }
--- SylvesterTests/MatrixTests.cs
+++ SylvesterTests/MatrixTests.cs
@@ -10,10 +10,8 @@ namespace SylvesterTests
         public void MatrixInitializesToZerosByDefault()
         {
             var m = new Matrix(2, 2);
-            Assert.That(m.GetElement(0, 0), Is.EqualTo(0));
-            Assert.That(m.GetElement(0, 1), Is.EqualTo(0));
-            Assert.That(m.GetElement(1, 0), Is.EqualTo(0));
-            Assert.That(m.GetElement(1, 1), Is.EqualTo(0));
+            Assert.That(m.GetRow(0), Is.EqualTo(new double[] { 0, 0 }));
+            Assert.That(m.GetRow(1), Is.EqualTo(new double[] { 0, 0 }));
         }
 
         [Test]
@@ -22,19 +20,66 @@ namespace SylvesterTests
             var m = new Matrix(2, 2);
             m.SetElement(0, 0, 1);
             Assert.That(m.GetElement(0, 0), Is.EqualTo(1));
-            Assert.That(m.GetElement(0, 1), Is.EqualTo(0));
-            Assert.That(m.GetElement(1, 0), Is.EqualTo(0));
-            Assert.That(m.GetElement(1, 1), Is.EqualTo(0));
         }
 
         [Test]
         public void CanSetRowByRowIndex()
         {
             var m = new Matrix(3, 3);
-            m.SetElements(1, new double[3] {1,2,3});
-            Assert.That(m.GetElement(1, 0), Is.EqualTo(1));
-            Assert.That(m.GetElement(1, 1), Is.EqualTo(2));
-            Assert.That(m.GetElement(1, 2), Is.EqualTo(3));
+            m.SetRow(1, new double[] { 1, 2, 3 });
+            Assert.That(m.GetRow(1), Is.EqualTo(new double[] { 1, 2, 3 }));
+        }
+
+        [Test]
+        public void CanSetColumnByColumnIndex()
+        {
+            var m = new Matrix(3, 3);
+            m.SetColumn(1, new double[] { 1, 2, 3 });
+            Assert.That(m.GetColumn(1), Is.EqualTo(new double[] { 1, 2, 3 }));
+        }
+
+        [Test]
+        public void EqualIfSameSizeAndAllElementsAreSame()
+        {
+            var m1 = new Matrix(2, 2);
+            m1.SetRow(0, new double[] { 0, 1 });
+            m1.SetRow(1, new double[] { 2, 3 });
+
+            var m2 = new Matrix(2, 2);
+            m2.SetRow(0, new double[] { 0, 1 });
+            m2.SetRow(1, new double[] { 2, 3 });
+
+            Assert.True(m1 == m2);
+            Assert.True(m1.Equals(m2));
+        }
+
+        [Test]
+        public void NotEqualIfDifferentSize()
+        {
+            var m1 = new Matrix(3, 2);
+            m1.SetRow(0, new double[] { 0, 1 });
+            m1.SetRow(1, new double[] { 2, 3 });
+            m1.SetRow(2, new double[] { 4, 5 });
+
+            var m2 = new Matrix(2, 2);
+            m2.SetRow(0, new double[] { 0, 1 });
+            m2.SetRow(1, new double[] { 2, 3 });
+
+            Assert.True(m1 != m2);
+        }
+
+        [Test]
+        public void NotEqualIfDifferentValuesForElements()
+        {
+            var m1 = new Matrix(2, 2);
+            m1.SetRow(0, new double[] { 1, 1 });
+            m1.SetRow(1, new double[] { 2, 3 });
+
+            var m2 = new Matrix(2, 2);
+            m2.SetRow(0, new double[] { 0, 1 });
+            m2.SetRow(1, new double[] { 2, 3 });
+
+            Assert.True(m1 != m2);
         }
     }
 }
