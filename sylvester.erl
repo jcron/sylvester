b90a7500f5496511005f666aadd9d1846348b521
Jeremy <jeremykentc@gmail.com>
2013-01-22 08:02:36 -0500

    adding basic unit test for hello_world/0

--- sylvester.erl
+++ sylvester.erl
@@ -2,5 +2,13 @@
 
 -export([hello_world/0]).
 
+-include_lib("eunit/include/eunit.hrl").
+
 hello_world() ->
-        "hello world!".
+    "hello world!".
+
+
+%%% EUNIT Tests
+hello_world_test() ->
+    "hello world!" =:= hello_world().
+
