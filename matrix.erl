30756fc88f67fc9b70cea2b724839d6f107c0dbe
Jeremy <jeremykentc@gmail.com>
2013-01-23 08:02:33 -0500

    completing a default matrix to be built and getting the tests to pass.

--- matrix.erl
+++ matrix.erl
@@ -1,12 +1,12 @@
 -module(matrix).
 
--export([default/2]).
+-export([default/2,
+         print/0]).
 
 -include_lib("eunit/include/eunit.hrl").
 
 default(Rows, Columns) when is_integer(Rows), is_integer(Columns) ->
-    %build_matrix(0, Rows, Columns, []).
-    [[0,0],[0,0]].
+    build_matrix(Rows, Columns).
 
 build_list(Size) ->
     build_list(Size, []).
@@ -16,24 +16,34 @@ build_list(0, List) ->
 build_list(Size, List) ->
     build_list(Size - 1, [0 | List]).
 
+build_matrix(Rows, Columns) ->
+    build_matrix(Rows, Columns, []).
+
+build_matrix(0, 0, Matrix) ->
+    Matrix;
+build_matrix(Rows, Columns, Matrix) ->
+    append_list_to_matrix(Rows, build_list(Columns, Matrix), Matrix).
+    
+append_list_to_matrix(0, _List, Matrix) ->
+    Matrix;
+append_list_to_matrix(Rows, List, Matrix) ->
+    append_list_to_matrix(Rows - 1, List, [List | Matrix]).
+    
+print() ->
+    io:format("~p", [build_matrix(3,3)]).
 %%% EUNIT Tests
 default_has_all_zeros_test() ->
     ?assert([[0,0],[0,0]] =:= default(2,2)).
     
+default_jagged_test() ->
+    ?assert([[0,0,0],[0,0,0]] =:= default(2,3)).
+    
 default_needs_integers_test() ->
     ?assertError(function_clause, default('Foo', 2)),
     ?assertError(function_clause, default(2, 'Bar')).
+    
 build_list_test() ->
     ?assert([0,0,0] =:= build_list(3)).
-%default_jagged_test() ->
-    %?assert([[0,0],[0,0,0]] =:= default(2,3)).
-
-%        public Matrix(int rows, int columns)
-%        {
-%            _matrix = new double[rows, columns];
-%            _rows = rows;
-%            _columns = columns;
-%            for (var i = 0; i < rows; i++)          For every row
-%                for (var j = 0; j < columns; j++)   For every column
-%                    _matrix[i, j] = 0;              Initialize the row, column intersection with zero
-%        }
+    
+build_matrix_test() ->
+    ?assert([[0,0,0],[0,0,0],[0,0,0]] =:= build_matrix(3, 3)).
